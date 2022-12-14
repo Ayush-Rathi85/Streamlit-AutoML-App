5ea48101cecb87884d3e7ab5f1832a2ea9161189
Ayush Rathi <rathiayush85@gmail.com>
2022-12-14 12:24:22 +0530

    Full APP development is completed.

--- main.py
+++ main.py
@@ -1,5 +1,69 @@
+# importing required Libraries
 import pandas as pd
+from streamlit_pandas_profiling import st_profile_report
 import streamlit as st
+import os
 
-for i in range(1,20):
-    print("i am legend")
+# Creating Menu bar
+with st.sidebar:
+    st.image('image.png')
+    st.title("AutoML App")
+    choice = st.radio("Menu", ['Upload Data', 'Data Profile', 'ML Model Training', 'Download Model'])
+    st.info("This application builds machine learning model automatically using pycaret, pandas profiling and streamlit")
+
+# Reading file if saved locally
+if os.path.exists('file.csv'):
+    df = pd.read_csv('file.csv')
+
+# Uploading data in our app
+if choice == 'Upload Data':
+    st.title("Upload your data for modelling")
+    file = st.file_uploader("Upload your dataset (it should be in csv format)")
+    if file:
+        df = pd.read_csv(file, index_col=False)
+        df.to_csv('file.csv', index=False)
+        st.dataframe(df)
+
+# Using Pandas data profiling to see the Exploretory data analysis
+if choice == 'Data Profile':
+    st.title("Automated EDA")
+    data_profile = df.profile_report()
+    st_profile_report(data_profile)
+
+# Training model according to users choice
+if choice == 'ML Model Training':
+    st.title("Model Training")
+    target = st.selectbox("Select your target", df.columns) # Selecting Target class for model training
+    Model_type = st.radio("Select Type of Model", ['Regression', 'Classification']) # choosing type of ML Problem
+
+# Passing the data to the selected problem type
+    if Model_type == 'Regression': # for regression problem
+        from pycaret.regression import setup, compare_models, pull, save_model
+        setup(df, target=target, silent=True)
+        if st.button("Train Model"):
+            setup_df = pull()
+            st.info("These are the ML Model testing settings")
+            st.dataframe(setup_df)
+            best_model = compare_models()
+            compare_df = pull()
+            st.info("The best model for the data set: ")
+            st.dataframe(compare_df)
+            save_model(best_model, 'best_model')
+
+    if Model_type == 'Classification': # for classification problem
+        from pycaret.classification import setup, compare_models, pull, save_model
+        setup(df, target=target, silent=True)
+        if st.button("Train Model"):
+            setup_df = pull()
+            st.info("These are the ML Model testing settings")
+            st.dataframe(setup_df)
+            best_model = compare_models()
+            compare_df = pull()
+            st.info("The best model for the data set: ")
+            st.dataframe(compare_df)
+            save_model(best_model,'best_model')
+
+# Providing Downloadable ML model
+if choice == 'Download Model':
+    with open("best_model.pkl", 'rb') as f:
+        st.download_button('Download the model', f, "Auto_ML_Model.pkl")
